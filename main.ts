109b8aa5b19bc9c6a1549cbaa926524efd93d901
rebecalasuenremon <98742881+rebecalasuenremon@users.noreply.github.com>
2022-03-28 11:27:13 +0200

    kk

--- main.ts
+++ main.ts
@@ -1,19 +1,20 @@
 basic.forever(function () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
-    } else {
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            maqueen.motorStop(maqueen.Motors.All)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
-        }
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
-            maqueen.motorStop(maqueen.Motors.M1)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
-        }
-    }
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) == 4) {
         maqueen.motorStop(maqueen.Motors.All)
+    } else {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+        } else {
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+                maqueen.motorStop(maqueen.Motors.All)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+            }
+            if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+                maqueen.motorStop(maqueen.Motors.M1)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+            }
+        }
     }
 })
